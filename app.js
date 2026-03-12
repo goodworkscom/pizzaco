948e26cf07a89f6f736bd09808ab9e6f4c60ebc5
goodworkscom <goodworksco7@gmail.com>
2026-03-13 02:22:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-const supabase = supabase.createClient(
+const { createClient } = supabase;
+
+const supabaseClient = createClient(
 "https://vvetymfsfggvtrvglhrj.supabase.co",
 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2ZXR5bWZzZmdndnRydmdsaHJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzMzY0MjgsImV4cCI6MjA4ODkxMjQyOH0.RNvUimqkeWJo4SBJhmAipf2-XW-eVvgBwfV2hLo6-5k"
 );
+
 let orderNumber = Math.floor(Math.random()*900)+100;
 
 function placeOrder(){
 
 let name = document.getElementById("name").value;
 
-alert("Order placed! Your number is "+orderNumber);
+alert("Order placed! Your number is " + orderNumber);
 
 localStorage.setItem(orderNumber,"preparing");
 
 }
+
 async function testOrder(){
 
 const { data, error } = await supabaseClient
