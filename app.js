71c167e98db8329fe6d100a7a0c80d282258c5eb
goodworkscom <goodworksco7@gmail.com>
2026-03-13 01:41:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+let orderNumber = Math.floor(Math.random()*900)+100;
 
+function placeOrder(){
+
+let name = document.getElementById("name").value;
+
+alert("Order placed! Your number is "+orderNumber);
+
+localStorage.setItem(orderNumber,"preparing");
+
+}
