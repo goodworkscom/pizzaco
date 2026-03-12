bf0457cd2f6d734470eb525ad10a2816eb1c8755
goodworkscom <goodworksco7@gmail.com>
2026-03-13 02:11:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const supabase = supabase.createClient(
+"https://vvetymfsfggvtrvglhrj.supabase.co",
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2ZXR5bWZzZmdndnRydmdsaHJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzMzY0MjgsImV4cCI6MjA4ODkxMjQyOH0.RNvUimqkeWJo4SBJhmAipf2-XW-eVvgBwfV2hLo6-5k"
+);
 let orderNumber = Math.floor(Math.random()*900)+100;
 
 function placeOrder(){
